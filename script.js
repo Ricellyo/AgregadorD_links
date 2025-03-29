f5088d4794bfc6760b667851870378cfa34c1d36
Ricellyo Cruz Silva <ricellyocruzsilva@gmail.com.br>
2025-03-29 06:21:59 -0300

    melhorando a logica

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function toggleMode() {
   const html = document.documentElement;
 
-  if(html.classlist.contains('light')) {
-    html.classlist.add('light')
-  } else {
-    html.classlist.remove('light')
-  }
+  // if(html.classlist.contains('light')) {
+  //   html.classlist.add('light')
+  // } else {
+  //   html.classlist.remove('light')
+  // }
+  html.classList.toggle('light');
 }
 
