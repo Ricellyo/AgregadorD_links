08c45dea0e398ba6b8c17ee4896b3a879af5b3cb
Ricellyo Cruz Silva <ricellyocruzsilva@gmail.com.br>
2025-03-31 05:47:28 -0300

    Atualizando atributos, e preparando para mais atalizações na imagem light

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ function toggleMode() {
   // } else {
   //   html.classlist.remove('light')
   // }
+  
   html.classList.toggle('light');
+
+  // trocando a imagem
+  // const img = document.querySelector("#profile img"):
+
+  // if(html.classList.contains('light')){
+  //   img.setAttibute("src","assets/Avatar.png");
+  //   img.setAttibute("alt","foto de ricellyo de oculos, blusão azul e fundo decorativos de luzes do shopping");
+  // } else {
+  //   img.setAttibute("src","assets/Avatar.png");
+  //   img.setAttibute("alt","foto de ricellyo de oculos, blusão azul e fundo decorativos de luzes do shopping");
+
+  // }
+  // OBS: Estou repetindo a img devido n ter uma prnova pronta
+
 }
 
